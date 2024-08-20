87f8159ea6795ba322416aa20a85430299c91bfb
DannyNak2 <danielnakachew1@gmail.com>
2024-08-20 11:57:18 +0300

    updated models.py, views.py and create a readme file in bookshelf directory

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Book
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
 
 # Register your models here.
 @admin.register(Book)
@@ -21,3 +23,28 @@ class CustomUserAdmin(UserAdmin):
     )
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
+
+# Create groups and assign permissions
+def setup_groups():
+    # Create groups
+    editors_group, created = Group.objects.get_or_create(name='Editors')
+    viewers_group, created = Group.objects.get_or_create(name='Viewers')
+    admins_group, created = Group.objects.get_or_create(name='Admins')
+
+    # Get permissions for the Book model
+    content_type = ContentType.objects.get_for_model(Book)
+    can_view = Permission.objects.get(codename='can_view', content_type=content_type)
+    can_create = Permission.objects.get(codename='can_create', content_type=content_type)
+    can_edit = Permission.objects.get(codename='can_edit', content_type=content_type)
+    can_delete = Permission.objects.get(codename='can_delete', content_type=content_type)
+
+    # Assign permissions to groups
+    editors_group.permissions.set([can_create, can_edit])
+    viewers_group.permissions.set([can_view])
+    admins_group.permissions.set([can_view, can_create, can_edit, can_delete])
+
+    # Optional: Register function to run on application start
+    # setup_groups()
+
+admin.site.register(Book)
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Book
 
-# Create your views here.
+@permission_required('relationship_app.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/book_list.html', {'books': books})
+
+@permission_required('relationship_app.can_create', raise_exception=True)
+def book_create(request):
+    if request.method == 'POST':
+        # Process form data and create the book
+        return redirect('book_list')
+    return render(request, 'relationship_app/book_form.html')
+
+@permission_required('relationship_app.can_edit', raise_exception=True)
+def book_edit(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        # Process form data and edit the book
+        return redirect('book_list')
+    return render(request, 'relationship_app/book_form.html', {'book': book})
+
+@permission_required('relationship_app.can_delete', raise_exception=True)
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'relationship_app/book_confirm_delete.html', {'book': book})
+
+
