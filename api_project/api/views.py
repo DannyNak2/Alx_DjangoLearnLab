5791772c2664f28625b3b05d96ff49a3aa9a1819
DannyNak2 <danielnakachew1@gmail.com>
2024-09-01 21:13:22 +0300

    edited views

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsAuthorOrReadOnly  # Import your custom permission
