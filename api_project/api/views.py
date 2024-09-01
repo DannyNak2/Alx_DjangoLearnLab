c21831fe6db5d86dc632ad16e37034a46a67a215
DannyNak2 <danielnakachew1@gmail.com>
2024-09-01 20:54:34 +0300

    new projects

---
+++ api_project/api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsAuthorOrReadOnly  # Import your custom permission
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]  # Apply permissions
